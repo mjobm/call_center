f45750b100921a799d45b4ac92fa6487fbb2b73e
Joseph <joseph.kimani@busaracenter.org>
2016-07-25 11:59:16 +0300

    templates initial commit

--- call_center/urls.py
+++ call_center/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    url(r'', include('call_center.urls', namespace="index")),
+    url(r"^accounts/login/$", auth_views.login, name="login"),
+    url(r"^accounts/logout/$", auth_views.logout, {"next_page": "/"}, name="logout"),
     url(r'^admin/', admin.site.urls),
 ]
